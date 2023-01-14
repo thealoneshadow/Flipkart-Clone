5b6d8729f8ab82858c2ef881d5f933ff24ea2d86
Divyanshu Goyal <divyanshu.goyal@cybergindia.local>
2023-01-14 19:10:52 +0530

    Order Tracking for admin

--- admin-frontend/src/actions/initialData.action.js
+++ admin-frontend/src/actions/initialData.action.js
@@ -5,12 +5,13 @@ import {
 	initialDataConstants,
 	categoryConstants,
 	productConstants,
+	orderConstants,
 } from "./constants";
 export const getInitialData = () => {
 	return async (dispatch) => {
 		const res = await axios.post("/initialdata");
 		if (res.status === 200) {
-			const { categories, products } = res.data;
+			const { categories, products, orders } = res.data;
 			dispatch({
 				type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
 				payload: { categories },
@@ -18,6 +19,10 @@ export const getInitialData = () => {
 			dispatch({
 				type: productConstants.GET_ALL_PRODUCTS_SUCCESS,
 				payload: { products },
+			});
+			dispatch({
+				type: orderConstants.GET_CUSTOMER_ORDER_SUCCESS,
+				payload: { orders },
 			});
 		} else {
 			dispatch({
---
+++ admin-frontend/src/components/UI/Card/Card.js
@@ -0,0 +1,26 @@
+/** @format */
+
+import React from "react";
+import "./Card.css";
+
+/**
+ * @author
+ * @function Card
+ **/
+
+const Card = (props) => {
+	return (
+		<div className="card" {...props}>
+			{(props.headerLeft || props.headerRight) && (
+				<div className="cardHeader">
+					{props.headerLeft && <div>{props.headerLeft}</div>}
+					{props.headerRight && props.headerRight}
+				</div>
+			)}
+
+			{props.children}
+		</div>
+	);
+};
+
+export default Card;
